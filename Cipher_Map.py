ba1e121fd7415d8d0b3410bdf2f327bdcc643131
Gainutdinov <m.gaynutdinov@mail.ru>
2017-08-13 23:08:41 +0300

    for R2 works

--- Cipher_Map.py
+++ Cipher_Map.py
@@ -1,12 +1,45 @@
 def recall_password(cipher_grille, ciphered_password):
+    decrypted_word = ""
     def rotate_90degree(matrix):
-        return 0
+        rotated90 = list(zip(*matrix[::-1]))
+        return rotated90
     def convert_to_matrix(data):
-        return 0
+        # Creates a list containing 4 lists, each of 4 items, all set to 0
+        row, column = len(cipher_grille), len(cipher_grille[0])
+        matrix = [[0 for x in range(row)] for y in range(column)]
+        row = 0
+        column = 0
+        for words in data:
+            for letter in words:
+                matrix[row][column] = letter
+                column +=1
+            row += 1
+            column = 0
+        return matrix
+
     def decrypt(grille, template, hole='X'):
-        return 0
+        decoded_word = ""
+        row = 0
+        column = 0
+        for x in grille:
+            for y in x:
+                if y == hole:
+                    decoded_word += template[row][column]
+                column +=1
+            row += 1
+            column = 0   
+        return decoded_word
     
-    return ""
+
+    word_matrix = convert_to_matrix(ciphered_password)
+    ciphered_matrix = convert_to_matrix(cipher_grille)
+    for i in range(4):
+        decrypted_word += decrypt(ciphered_matrix, word_matrix)
+        ciphered_matrix = rotate_90degree(ciphered_matrix)
+    print(decrypted_word)       
+    return decrypted_word
+
+
 
 
 if __name__ == '__main__':
