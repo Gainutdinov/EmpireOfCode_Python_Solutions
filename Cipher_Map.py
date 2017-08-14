73cfb16cbe22f5b74725163c2292b9798d5b9a35
Gainutdinov <m.gaynutdinov@mail.ru>
2017-08-14 12:10:30 +0300

    works for three stars

--- Cipher_Map.py
+++ Cipher_Map.py
@@ -1,8 +1,17 @@
 def recall_password(cipher_grille, ciphered_password):
+
     decrypted_word = ""
+
     def rotate_90degree(matrix):
         rotated90 = list(zip(*matrix[::-1]))
         return rotated90
+
+    def rotate_minus90degree(matrix):
+        rotated90 = list(zip(*matrix[::-1]))
+        rotated90 = list(zip(*rotated90[::-1]))
+        rotated90 = list(zip(*rotated90[::-1]))
+        return rotated90
+
     def convert_to_matrix(data):
         # Creates a list containing 4 lists, each of 4 items, all set to 0
         row, column = len(cipher_grille), len(cipher_grille[0])
@@ -12,7 +21,7 @@ def recall_password(cipher_grille, ciphered_password):
         for words in data:
             for letter in words:
                 matrix[row][column] = letter
-                column +=1
+                column += 1
             row += 1
             column = 0
         return matrix
@@ -25,64 +34,23 @@ def recall_password(cipher_grille, ciphered_password):
             for y in x:
                 if y == hole:
                     decoded_word += template[row][column]
-                column +=1
+                column += 1
             row += 1
-            column = 0   
+            column = 0
         return decoded_word
-    
-
     word_matrix = convert_to_matrix(ciphered_password)
     ciphered_matrix = convert_to_matrix(cipher_grille)
-    for i in range(4):
-        decrypted_word += decrypt(ciphered_matrix, word_matrix)
-        ciphered_matrix = rotate_90degree(ciphered_matrix)
-    print(decrypted_word)       
-    return decrypted_word
+    if ((ciphered_password[0])[0].isupper()):
+        for i in range(4):
+            decrypted_word += decrypt(ciphered_matrix, word_matrix)
+            ciphered_matrix = rotate_minus90degree(ciphered_matrix)
+    else:
+        for i in range(4):
+            decrypted_word += decrypt(ciphered_matrix, word_matrix)
+            ciphered_matrix = rotate_90degree(ciphered_matrix)
+        return decrypted_word
 
-
-
-
-if __name__ == '__main__':
-    # These "asserts" using only for self-checking and not necessary for auto-testing
-    assert recall_password(
-        ('X...',
-         '..X.',
-         'X..X',
-         '....'),
-        ('itdf',
-         'gdce',
-         'aton',
-         'qrdi')) == 'icantforgetiddqd', 'First example'
-
-    assert recall_password(
-        ('....',
-         'X..X',
-         '.X..',
-         '...X'),
-        ('xhwc',
-         'rsqx',
-         'xqzz',
-         'fyzr')) == 'rxqrwsfzxqxzhczy', 'Second example'
-
-    # Rank 2
-    assert recall_password(
-        ('.X...X.',
-         'X.....X',
-         '.......',
-         '...X...',
-         '.......',
-         'X.....X',
-         '.X...X.'),
-        ('loremip',
-         'sumdolo',
-         'rsitame',
-         'tconsec',
-         'teturad',
-         'ipiscin',
-         'gelitqu')) == "oisonineqoisonineqoisonineqoisonineq", "R2"
-
-    # Rank 3
-    assert recall_password(
+recall_password(
         ('.X...',
          '.X...',
          '..X..',
@@ -92,6 +60,4 @@ if __name__ == '__main__':
          'ASDFG',
          'ZXCVB',
          'YUIOP',
-         'GHJKL')) == "WSCUHCYUOPRFCOKASFGC", "R3"
-
-    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+         'GHJKL'))
