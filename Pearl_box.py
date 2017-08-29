d2d912bf74937c2bd7f6c980d0162cdf71e51400
Gainutdinov <m.gaynutdinov@mail.ru>
2017-08-29 22:13:07 +0300

    corrected a little bit.

--- Pearl_box.py
+++ Pearl_box.py
@@ -2,17 +2,18 @@ def probability(sequence, step):
     w = sequence.count('w')
     b = sequence.count('b')
     s = step
+
     def probability_finder(white, black, step):
-        white_prob = white / (white+black)
+        white_prob = white / (white + black)
         black_prob = 1 - white_prob
         if step == 1:
-            return round(white_prob,2)
+            return round(white_prob, 2)
 
         else:
-            pick_white = white_prob * probability_finder(white-1, black+1, step-1)
-            pick_black = black_prob * probability_finder(white+1, black-1, step-1)
-            return round((pick_white + pick_black),2)
+            pick_white = white_prob * \
+                probability_finder(white - 1, black + 1, step - 1)
+            pick_black = black_prob * \
+                probability_finder(white + 1, black - 1, step - 1)
+            return round((pick_white + pick_black), 3)
 
     return probability_finder(w, b, s)
-
-print(probability('bbw', 3))
